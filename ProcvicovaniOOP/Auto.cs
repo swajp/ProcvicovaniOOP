2fbf5c5283111eba7a9233f21a4344988e67d0a6
Miroslav Stejskal <Stejskal.Miroslav@stud.vassboskovice.cz>
2021-11-02 13:16:36 +0100

    Přidejte soubory projektu.

---
+++ ProcvicovaniOOP/Auto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProcvicovaniOOP
+{
+    public class Auto
+    {
+        private int naklad;
+        public int Naklad { get => naklad; }
+        public string Znacka { get; }
+        public int Nosnost { get; }
+
+        public Auto(string znacka, int nostnost)
+        {
+            Znacka = znacka;
+            Nosnost = nostnost;
+        }
+        public int Nalozit(int naklad)
+        {
+            var misto = Nosnost - Naklad;
+            if (misto < naklad)
+            {
+                this.naklad += misto;
+                return misto;
+            }
+            else
+            {
+                this.naklad += naklad;
+                return naklad;
+            }
+        }
+        public int Vylozit (int naklad)
+        {
+            var misto = Nosnost - Naklad;
+            if (misto < naklad)
+            {
+                this.naklad -= misto;
+                return misto;
+            }
+            else
+            {
+                this.naklad -= naklad;
+                return naklad;
+            }
+        }
+    }
+}
